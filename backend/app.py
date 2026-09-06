2fb97554a09da94506bc83b908a49560f81c16a8
Varun Yelisetty <varunyelisetty@Varuns-MacBook-Air.local>
2026-09-06 20:36:13 +0530

    Prepare FastAPI backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,9 @@ from tempfile import TemporaryDirectory
 from typing import Any, Dict
 from zipfile import BadZipFile, ZipFile, is_zipfile
 import tarfile
+import os
 
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import (
     FastAPI,
     File,
